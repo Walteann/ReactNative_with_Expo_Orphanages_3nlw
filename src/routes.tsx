fcec7a016da7b5bfb6faa03ac253a272c11cfffd
Walteann Costa da Silva <wcsilva@apoioprodesp.sp.gov.br>
2020-10-16 21:54:04 -0300

    React Native Orphanages #3 nlw

---
+++ src/routes.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+
+import Header from './components/Header';
+
+const { Navigator, Screen } = createStackNavigator();
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' }}}>
+                <Screen name="OrphanagesMap" component={OrphanagesMap}></Screen>
+                <Screen 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Orphanato"></Header>
+                    }}
+                    ></Screen>
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione no Mapa"></Header>
+                    }}
+                    ></Screen>
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os Dados"></Header>
+                    }}
+                ></Screen>
+            </Navigator>
+        </NavigationContainer>
+    );
+}
